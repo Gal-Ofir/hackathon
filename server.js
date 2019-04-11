b8d8c5395e1d77d142dd45cd3b62b65871332a1e
Alon Weissfeld <alonweissfeld@gmail.com>
2019-04-12 02:49:29 +0300

    Added categories.

--- server.js
+++ server.js
@@ -8,8 +8,24 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 app.use('/', express.static('public'));
+app.set('view engine', 'ejs');
+
+app
+    .get('/', function(req, res) {
+        res.render('index')
+    })
+    .get('/categories', function(req, res) {
+        res.render('categories')
+    })
+    .get('/train/:genre', function(req, res) {
+        let agentName = req.params.genre
+        res.render('train', {
+            questions: questions
+        })
+    })
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(routers);
+// app.use(routers);
 app.listen((process.env.PORT || port), () => console.log(`Example app listening on port ${port}!`));
