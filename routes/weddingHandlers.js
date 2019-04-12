3e06f5fe44a7ebb82e1c91dbe7d230732e50c240
Gal Ofir <gal.ofir.go@gmail.com>
2019-04-12 08:36:14 +0300

    oh yes

--- routes/weddingHandlers.js
+++ routes/weddingHandlers.js
@@ -40,6 +40,7 @@ const handleIntents = (req, res) => {
         const smsMessage = `Your reservation is confirmed! On ${date} at ${hour}:${minute}`;
         insertEvent(data);
         sendSms(smsMessage, '+972545222886');
+        sendSms(smsMessage, '+972545857707');
     }
     else {
         res.json({
